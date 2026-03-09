a0c7eca5db1ff7fed02e1c0fa1b4c61519b4c911
Ayaad <ayaad.contact@gmail.com>
2026-03-08 21:53:33 -0600

    fix: nvim-tree was not changing dirs properly

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,7 +4,12 @@ return {
     config = function()
         local tree = require("nvim-tree")
 
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
         tree.setup({
+            respect_buf_cwd = true,
+            sync_root_with_cwd = true,
             view = {
                 width = 35,
             },
