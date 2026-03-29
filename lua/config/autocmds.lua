7a02d2d988965b8a74c34d5df93b5019fb5cc6f6
Ayaad <ayaad.contact@gmail.com>
2026-03-29 16:54:00 -0600

    feat: remove autocomments when entering newlines

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.spell = true
 	end,
 })
+
+-- turn off comments continuing when placing them
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "r", "o" })
+	end,
+})
